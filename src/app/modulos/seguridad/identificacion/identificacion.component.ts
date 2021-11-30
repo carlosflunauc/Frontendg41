999d9b39d6998cbf3684de97fe5c41b3765ab2fd
carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>
2021-11-30 09:52:37 -0500

    identificacion sesion
    Signed-off-by: carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>

--- src/app/modulos/seguridad/identificacion/identificacion.component.ts
+++ src/app/modulos/seguridad/identificacion/identificacion.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { SeguridadService } from 'src/app/servicios/seguridad.service';
 import { Router } from '@angular/router';
+import * as CryptoJS from 'crypto-js';
 
 @Component({
   selector: 'app-identificacion',
@@ -26,8 +27,10 @@ export class IdentificacionComponent implements OnInit {
   IdentificarUsuario(){
     let usuario=this.fgValidador.controls["usuario"].value;
     let clave=this.fgValidador.controls["clave"].value;
-    alert(usuario)
-    alert(clave)
+    let claveCifrada = CryptoJS.MD5(clave).toString();
+    
+
+
   }
 
 }
--- src/app/servicios/seguridad.service.ts
+++ src/app/servicios/seguridad.service.ts
@@ -1,5 +1,6 @@
+import { ModeloIdentificar } from './../modelos/identificar.modelo';
 import { Injectable } from '@angular/core';
-import { from } from 'rxjs';
+import { BehaviorSubject, from, Observable, retry } from 'rxjs';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable({
@@ -8,8 +9,46 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 export class SeguridadService {
   
   url='http://127.0.0.1:3000';
+  datosUsuarioEnSesion = new BehaviorSubject<ModeloIdentificar>(new ModeloIdentificar())
 
   constructor(private http: HttpClient) {
-    
+    this.VerificarSesionActual();
+   }
+
+  VerificarSesionActual(){
+    let datos = this.ObtenerInformacionSesion();
+    if(datos){
+      this.RefrescarDatosSesion(datos);
+    }
+  }
+
+  RefrescarDatosSesion(datos: ModeloIdentificar){
+    this.datosUsuarioEnSesion.next(datos);
+  }
+
+  ObtenerDatosUsuarioEnSesion(){
+    return this.datosUsuarioEnSesion.asObservable();
+  }
+
+   Identificar(usuario: string, clave: string):Observable<ModeloIdentificar>{
+     return this.http.post<ModeloIdentificar>(`${this.url}/identificarUsuario`,{
+       usuario:usuario,
+       clave:clave
+     },{
+       headers: new HttpHeaders({
+
+       })
+     })
+   }
+   
+   ObtenerInformacionSesion(){
+     let datosString = localStorage.getItem("datosSesion");
+     if(datosString){
+       let datos = JSON.parse(datosString);
+       return datos;
+
+     }else{
+      return null;
+     }
    }
 }
