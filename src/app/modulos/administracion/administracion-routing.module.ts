59493852d1b3c3d310cd517d2b62406c65954b65
carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>
2021-11-30 08:55:44 -0500

    crearcion de rutas administrador y crear componentes en el modulo seguridad
    Signed-off-by: carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>

--- src/app/modulos/administracion/administracion-routing.module.ts
+++ src/app/modulos/administracion/administracion-routing.module.ts
@@ -1,3 +1,9 @@
+import { EliminarVehiculoComponent } from './vehiculos/eliminar-vehiculo/eliminar-vehiculo.component';
+import { BuscarVehiculoComponent } from './vehiculos/buscar-vehiculo/buscar-vehiculo.component';
+import { EditarVehiculoComponent } from './vehiculos/editar-vehiculo/editar-vehiculo.component';
+import { CrearVehiculoComponent } from './vehiculos/crear-vehiculo/crear-vehiculo.component';
+import { EliminarUsuarioComponent } from './usuarios/eliminar-usuario/eliminar-usuario.component';
+import { BuscarUsuarioComponent } from './usuarios/buscar-usuario/buscar-usuario.component';
 import { EditarUsuarioComponent } from './usuarios/editar-usuario/editar-usuario.component';
 import { CrearUsuarioComponent } from './usuarios/crear-usuario/crear-usuario.component';
 import { NgModule } from '@angular/core';
@@ -11,6 +17,33 @@ const routes: Routes = [
   {
   path: 'editar-usuario',
   component: EditarUsuarioComponent
+  },
+  {
+    path: 'buscar-usuario',
+    component: BuscarUsuarioComponent
+  },
+  {
+    path: 'eliminar-usuario',
+    component: EliminarUsuarioComponent
+  },
+
+  //componentes Vehiculos
+
+  {
+    path: 'crea-vehiculo',
+    component: CrearVehiculoComponent
+  },
+  {
+  path: 'editar-vehiculo',
+  component: EditarVehiculoComponent
+  },
+  {
+    path: 'buscar-vehiculo',
+    component: BuscarVehiculoComponent
+  },
+  {
+    path: 'eliminar-vehiculo',
+    component: EliminarVehiculoComponent
   }
 ];
 
