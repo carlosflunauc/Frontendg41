881d6e83e8bc2c9ae12c0ed58c0f6dbce10379c3
carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>
2021-11-30 09:30:28 -0500

    verificacion del modelo identificarUsuario
    Signed-off-by: carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>

--- src/app/modulos/seguridad/seguridad-routing.module.ts
+++ src/app/modulos/seguridad/seguridad-routing.module.ts
@@ -1,3 +1,6 @@
+import { CerrarSesionComponent } from './cerrar-sesion/cerrar-sesion.component';
+import { RecuperarClaveComponent } from './recuperar-clave/recuperar-clave.component';
+import { CambioClaveComponent } from './cambio-clave/cambio-clave.component';
 import { IdentificacionComponent } from './identificacion/identificacion.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -6,6 +9,18 @@ const routes: Routes = [
   {
     path: 'identificacion',
     component: IdentificacionComponent
+  },
+  {
+    path: 'cambio-clave',
+    component: CambioClaveComponent
+  },
+  {
+    path: 'recuperar-clave',
+    component: RecuperarClaveComponent
+  },
+  {
+    path: 'cerrar-sesion',
+    component: CerrarSesionComponent
   }
 ];
 
---
+++ src/app/servicios/seguridad.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { from } from 'rxjs';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SeguridadService {
+  
+  url='http://127.0.0.1:3000';
+
+  constructor(private http: HttpClient) {
+    
+   }
+}
